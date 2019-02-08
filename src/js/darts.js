f70230bcd359e4c5c299cee050b02de0bd953907
Jon Zielenkievicz <jonzielen@gmail.com>
2019-02-08 16:17:54 -0500

    update board

--- src/js/darts.js
+++ src/js/darts.js
@@ -1,10 +1,32 @@
 import "../sass/main.scss";
 
 (function() {
-  console.log('JOM???');
-  var jom100 = 'tghis is a long string of text to see if this works zzz';
-  console.log(jom100);
+  var startGame = document.getElementById('start-game'),
+      intro = document.getElementById('intro'),
+      game = document.getElementById('game'),
+      results = document.getElementById('results'),
+      gamePlayer1 = document.querySelector('#game .game-player1 h2'),
+      gamePlayer2 = document.querySelector('#game .game-player2 h2'),
+      scorePlayer1 = {},
+      scorePlayer2 = {};
 
-  var throop = 'all three';
-  console.log(throop);
+  startGame.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var player1 = document.getElementById('player1'),
+        player2 = document.getElementById('player2');
+
+    buildGame();
+  });
+
+  function buildGame() {
+    intro.classList.add('hidden');
+    game.classList.remove('hidden');
+
+    gamePlayer1.innerHTML = player1.value;
+    gamePlayer2.innerHTML = player2.value;
+
+    console.log('player1: ', player1, 'player2: ', player2);
+  }
+
 })();
